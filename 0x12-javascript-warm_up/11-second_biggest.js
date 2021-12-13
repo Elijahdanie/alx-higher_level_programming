2cfa19ba74b177766300cd25444f236a0dcd6510
Elijahdanie <elijahdanie@gmail.com>
2021-12-13 21:48:58 +0100

    ...

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 3) {
-    console.log('1');
+    console.log('0');
 } else {
     let arg = process.argv.slice(2);
     console.log(arg.sort(function (a, b) { return a - b })[1]);
