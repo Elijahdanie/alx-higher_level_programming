8f677621ad696d20de8af72ef4f752c6349cef57
Elijahdanie <elijahdanie@gmail.com>
2021-12-20 12:44:14 +0100

    pycodestyle compliance

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module fetches the state record indicated by the 
-state name from the command line argument, eleminating 
+This module fetches the state record indicated by the
+state name from the command line argument, eleminating
 vulnerability for SQL injections. %s symbol lets
 the entry get included seperately from the sql query as
 a value type, rather than a string type
@@ -15,8 +15,10 @@ if __name__ == '__main__':
                                 user=sys.argv[2],
                                 db=sys.argv[3])
     cursor = conn.cursor()
-    targetState =  sys.argv[4]
-    cursor.execute("select * from states where name = %s order by id ASC", (sys.argv[4],))
+    targetState = sys.argv[4]
+    cursor.execute(
+        "select * from states where name = %s order by id ASC",
+        (sys.argv[4],))
     result = cursor.fetchall()
     for i in result:
         print(i)
