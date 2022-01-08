bd0d927689b82c59da371d3d1e039cfe5fdab061
Elijahdanie <elijahdanie@gmail.com>
2022-01-08 10:08:06 +0100

    ....

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this list all http methods supported by URI passed in
-curl -sX OPTIONS $1 -i | grep Allow | awk -F ":" '{print $2}'
+curl -sX OPTIONS $1 -i | grep Allow | awk -F ":" '{print $2}' | cut -d ' ' -f 2-
