d9014d4e833e02f327dc276e44d67fbf508034e9
Elijahdanie <elijahdanie@gmail.com>
2022-01-15 01:47:23 +0100

    worked on peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module calculates the peak in an array of numbers
+"""
+
+def find_peak(list_of_integers):
+    """
+    This finds the peak from the array
+    """
+    if len(list_of_integers) == 0:
+        return None
+    peak = 0
+    left = 0
+    right = len(list_of_integers) - 1
+    iteration = int(len(list_of_integers)/2)
+    for i in range(iteration):
+        peak = isgreater(list_of_integers[left],
+        list_of_integers[right], peak)
+        right = right - 1
+        left = left + 1
+    return peak
+
+def isgreater(curr, comp1, peak):
+    """
+    This makes comparison between values
+    """
+    if curr > comp1 and curr >= peak:
+        return curr
+    elif comp1 > curr and comp1 >= peak:
+        return comp1
+    elif comp1 == curr and comp1 >= peak:
+        return curr
+    else:
+        return peak
