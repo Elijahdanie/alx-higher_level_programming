8f760fd315c25c9edcdbcae55a9eb3923edc237c
Elijahdanie <elijahdanie@gmail.com>
2022-01-15 02:20:34 +0100

    some fixes and optimization for effiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,33 +8,7 @@ def find_peak(list_of_integers):
     """
     This finds the peak from the array
     """
-    if len(list_of_integers) == 0:
-        return None
-    peak = 0
-    left = 0
-    right = len(list_of_integers) - 1
-    iteration = len(list_of_integers)/2
-    check_dec = str(iteration)
-    if (len(check_dec) == 3 and check_dec[-1] == '5'):
-        iteration = int(iteration) + 1
-    else:
-        iteration = int(iteration)
-    for i in range(iteration):
-        peak = isgreater(list_of_integers[left], list_of_integers[right], peak)
-        right = right - 1
-        left = left + 1
-    return peak
-
-
-def isgreater(curr, comp1, peak):
-    """
-    This makes comparison between values
-    """
-    if curr > comp1 and curr >= peak:
-        return curr
-    elif comp1 > curr and comp1 >= peak:
-        return comp1
-    elif comp1 == curr and comp1 >= peak:
-        return curr
-    else:
-        return peak
+    if(list_of_integers):
+        arr_len = len(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[arr_len - 1]
