b5afec664cae6681b2f85da2f690bd52a27dd05f
Elijahdanie <elijahdanie@gmail.com>
2022-01-25 14:22:20 +0100

    edited an issue

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --- grants privileges to the users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
