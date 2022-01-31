633c3a8ac7c174e5ca76e0b1018f86aec28a8181
Elijahdanie <elijahdanie@gmail.com>
2022-01-31 13:12:16 +0100

    ...

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         resp = requests.get('https://api.github.com/user', auth)
         jdata = resp.json()
         print(jdata['id'])
-    except BaseException:
+    except:
         print("None")
